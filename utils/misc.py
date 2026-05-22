3195faf2deb671c13ad82ca769e80a89a435f00f
Callum <callumdempseyirl@gmail.com>
2026-05-22 15:57:45 +0200

    Fix typo in einsum

--- utils/misc.py
+++ utils/misc.py
@@ -202,7 +202,7 @@ def pearson_diversity_loss_vectorised(all_preds):
 
     # Compute full [M, M] correlation matrix for each class C
     # normed [M, C, B*H*W] --> corr [C, M, M]
-    corr = torch.einsum('mcd,ndc->cnm', normed, normed) / (B * H * W)  # [C, M, M]
+    corr = torch.einsum('mcd,ncd->cmn', normed, normed) / (B * H * W)  # [C, M, M]
 
     # Average over classes
     corr_mean = corr.mean(dim=0)  # [M, M]
